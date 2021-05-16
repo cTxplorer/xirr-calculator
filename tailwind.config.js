025daf74cc30dc509e2b3e30ccf327b9bfa3a5d6
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2021-05-16 16:18:52 +0530

    build: purge unused tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      './pages/*.js',
+      './components/*.js'
+    ]
+  },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
