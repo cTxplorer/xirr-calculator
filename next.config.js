c588a314a442a3aaa6614c5a6c06667d628fff32
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2024-03-16 13:16:14 +0530

    chore: upgrade next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-const withOffline = require('next-offline');
+const nextConfig = {
+  output: 'export',
 
-module.exports = withOffline();
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
