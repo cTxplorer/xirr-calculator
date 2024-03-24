64b419de12d52a861d373ef70c303803f37e2bf7
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2024-03-24 20:51:07 +0530

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,29 @@
 import Head from 'next/head';
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
 
 import 'tailwindcss/tailwind.css';
 import '../static/css/global.css';
 import '../static/css/global-new.css';
 
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    },
+  })
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>XIRR Calculator</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
     </>
   );
 }
